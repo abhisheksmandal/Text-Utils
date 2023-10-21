9252b3dad37c2e22ca9fd5390f35456b9387b51b
Abhishek Mandal <abhishek.s.mandal@gmail.com>
2023-10-21 22:02:54 +0530

    Update 2

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,15 +1,24 @@
 import React, { useState } from "react";
 
 export default function TextForm() {
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
+
+  const handleUpClick = () => {
+    setText(text.toUpperCase());
+  };
+
+  const handleLowClick = () => {
+    setText(text.toLowerCase());
+  };
 
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
 
   const handleOnClear = () => {
-    setText('');
+    setText("");
   };
+
   return (
     <div className="container">
       <div className="mb-3">
@@ -26,18 +35,27 @@ export default function TextForm() {
       </div>
       <button
         type="button"
-        className="btn btn-primary"
-        onclick="{handleUpClick}"
+        className="btn btn-primary me-3"
+        onClick={handleUpClick}
       >
-        Click
+        Uppercase
       </button>
       <button
         type="button"
-        className="btn btn-primary mx-3"
-        onclick="{handleOnClear}"
+        className="btn btn-primary me-3"
+        onClick={handleLowClick}
+      >
+        Lowercase
+      </button>
+      <button
+        type="button"
+        className="btn btn-primary me-3"
+        onClick={handleOnClear}
       >
         Clear
       </button>
+      <h3 className="mt-3">Your text summary</h3>
+      <p>{text}</p>
     </div>
   );
 }
