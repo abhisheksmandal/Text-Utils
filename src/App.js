da84ce9d34110b797f6f94a8f5326711ccc97f72
Abhishek Mandal <abhishek.s.mandal@gmail.com>
2023-10-21 21:07:21 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar title="Text Utils" subNav="About" />
         <TextForm />
       </div>
     );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextForm() {
+  const [text, setText] = useState('');
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleOnClear = () => {
+    setText('');
+  };
   return (
     <div className="container">
       <div className="mb-3">
-        <label htmlFor="exampleFormControlInput1" className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div className="mb-3">
         <label htmlFor="exampleFormControlTextarea1" className="form-label">
-          Example textarea
+          <h1>Enter your text here</h1>
         </label>
         <textarea
           className="form-control"
           id="exampleFormControlTextarea1"
           rows={3}
-          defaultValue={""}
+          value={text}
+          onChange={handleOnChange}
         />
       </div>
+      <button
+        type="button"
+        className="btn btn-primary"
+        onclick="{handleUpClick}"
+      >
+        Click
+      </button>
+      <button
+        type="button"
+        className="btn btn-primary mx-3"
+        onclick="{handleOnClear}"
+      >
+        Clear
+      </button>
     </div>
   );
 }
